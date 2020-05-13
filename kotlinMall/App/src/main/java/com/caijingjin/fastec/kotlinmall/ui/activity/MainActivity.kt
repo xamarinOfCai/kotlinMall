17860efaf32478606a865052c63499530a3fda84
tangchaolizi2019 <wx_6debcb947157494093310800b296f0a7@git.code.tencent.com>
2020-05-13 23:00:08 +0800

    kotlin学习模块化的配置 通过在gradle里面引入isUserModule的变量，来动态判断启用哪个模块

---
+++ kotlinMall/App/src/main/java/com/caijingjin/fastec/kotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.caijingjin.fastec.kotlinmall.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caijingjin.fastec.kotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ kotlinMall/UserCenter/src/main/java/com/caijingjin/fastec/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.caijingjin.fastec.user.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caijingjin.fastec.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
